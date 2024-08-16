040f134ce15f2c70c99d89d70922ff5cf3d5009d
Rohitgajbhiye2005 <165143231+Rohitgajbhiye2005@users.noreply.github.com>
2024-08-16 14:43:53 +0530

    Time: 5 ms (100%), Space: 64 MB (88.19%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
@@ -1,19 +1,31 @@
 class Solution {
     public int maxDistance(List<List<Integer>> arrays) {
-        int maxDistance = 0;
-        int minVal = arrays.get(0).get(0);
-        int maxVal = arrays.get(0).get(arrays.get(0).size() - 1);
-        
-        for (int i = 1; i < arrays.size(); i++) {
-            List<Integer> array = arrays.get(i);
-            int first = array.get(0);
-            int last = array.get(array.size() - 1);
-            maxDistance = Math.max(maxDistance, Math.abs(last - minVal));
-            maxDistance = Math.max(maxDistance, Math.abs(maxVal - first));
-            minVal = Math.min(minVal, first);
-            maxVal = Math.max(maxVal, last);
+        int min = Integer.MAX_VALUE;
+        int minIdx = -1;
+        int secMin = min;
+        int max = Integer.MIN_VALUE;
+        int maxIdx = -1;
+        int secMax = max;
+        for (int i = 0; i < arrays.size(); i++) {
+            List<Integer> arr = arrays.get(i);
+            int curMin = arr.get(0);
+            int curMax = arr.get(arr.size() - 1);
+            if (min > curMin) {
+                secMin = min;
+                min = curMin;
+                minIdx = i;
+            } else if (secMin > curMin) {
+                secMin = curMin;
+            }
+
+            if (max < curMax) {
+                secMax = max;
+                max = curMax;
+                maxIdx = i;
+            } else if (secMax < curMax) {
+                secMax = curMax;
+            }
         }
-        
-        return maxDistance;
+        return minIdx == maxIdx ? Math.max(max-secMin, secMax-min) : max - min;
     }
 }
