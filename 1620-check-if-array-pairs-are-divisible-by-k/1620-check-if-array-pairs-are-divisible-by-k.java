90890dee62f23a0aaabb459e06f3c361337ace2a
Rohitgajbhiye2005 <165143231+Rohitgajbhiye2005@users.noreply.github.com>
2024-10-02 09:52:53 +0530

    Time: 4 ms (98.96%), Space: 56.3 MB (66.94%) - LeetHub

---
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public boolean canArrange(int[] arr, int k) {
+        int[] remainderCounts = new int[k];
+    
+        for (int num : arr) {
+            int remainder = ((num % k) + k) % k; 
+            remainderCounts[remainder]++;
+        }
+       
+        for (int i = 1; i < k; i++) {
+            if (remainderCounts[i] != remainderCounts[k - i]) {
+                return false; 
+            }
+        }
+        
+       
+        return remainderCounts[0] % 2 == 0;
+    }
+}
