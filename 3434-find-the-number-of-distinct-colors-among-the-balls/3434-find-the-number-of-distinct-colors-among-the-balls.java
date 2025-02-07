ab86d40c0089ffc90920f89c1500c7b17d902d58
Rohitgajbhiye2005 <165143231+Rohitgajbhiye2005@users.noreply.github.com>
2025-02-07 13:02:28 +0530

    Time: 43 ms (45.85%), Space: 79.6 MB (95.2%) - LeetHub

--- 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.java
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.java
@@ -1,29 +1,24 @@
-import java.util.*;
-
 class Solution {
     public int[] queryResults(int limit, int[][] queries) {
-        Map<Integer, Set<Integer>> mpp = new HashMap<>();
-        Map<Integer, Integer> mp = new HashMap<>();
-        ArrayList<Integer> ans = new ArrayList<>();
 
-        for (int[] it : queries) {
-            int ball = it[0], color = it[1];
+        Map<Integer, Integer> ballMap = new HashMap<>();
+        Map<Integer, Integer> colorMap = new HashMap<>();
+        int[] result = new int[queries.length];
+        int index = 0;
 
-            if (!mp.containsKey(ball)) {
-                mp.put(ball, color);
-                mpp.computeIfAbsent(color, k -> new HashSet<>()).add(ball);
-            } else {
-                int c = mp.get(ball);
-                mpp.get(c).remove(ball);
-                if (mpp.get(c).isEmpty()) {
-                    mpp.remove(c);
-                }
-                mp.put(ball, color);
-                mpp.computeIfAbsent(color, k -> new HashSet<>()).add(ball);
+        for (int[] query : queries) {
+            colorMap.put(query[1], colorMap.getOrDefault(query[1], 0) + 1);
+            if (ballMap.containsKey(query[0])) {
+                int color = ballMap.get(query[0]);
+                int val = colorMap.get(color);
+                if (val == 1)
+                    colorMap.remove(color);
+                else
+                    colorMap.put(color, val - 1);
             }
-            ans.add(mpp.size());
+            ballMap.put(query[0], query[1]);
+            result[index++] = colorMap.size();
         }
-        
-        return ans.stream().mapToInt(Integer::intValue).toArray();
+        return result;
     }
 }
