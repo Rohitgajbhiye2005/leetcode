7fb4b855718e801984bcc1ff09b533ee54af18f7
Rohitgajbhiye2005 <165143231+Rohitgajbhiye2005@users.noreply.github.com>
2025-04-24 19:56:48 +0530

    Time: 330 ms (5.23%), Space: 45.2 MB (47.27%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
@@ -3,24 +3,22 @@ import java.util.*;
 class Solution {
     public int countCompleteSubarrays(int[] nums) {
         int n = nums.length;
-        int total = 0;
-
-        Set<Integer> distinctSet = new HashSet<>();
-        for (int num : nums) {
-            distinctSet.add(num);
+        int cnt = 0;
+        Set<Integer>distinct=new HashSet<>();
+        for(int num:nums){
+            distinct.add(num);
         }
-        int totalDistinct = distinctSet.size();
-
-        for (int i = 0; i < n; i++) {
-            Set<Integer> subSet = new HashSet<>();
-            for (int j = i; j < n; j++) {
-                subSet.add(nums[j]);
-                if (subSet.size() == totalDistinct) {
-                    total++;
-                }
+        int height=distinct.size();
+        for(int i=0;i<n;i++){
+            Set<Integer>subset=new HashSet<>();
+            for(int j=i;j<n;j++){
+                subset.add(nums[j]);
+                 if(subset.size()==height){
+                cnt++;
             }
+            }
+           
         }
-
-        return total;
+       return cnt;
     }
 }
