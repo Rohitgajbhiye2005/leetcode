61a97b55bd5bbfeba3c8ad3c5d82bbc6ab6d589f
Rohitgajbhiye2005 <165143231+Rohitgajbhiye2005@users.noreply.github.com>
2025-05-19 09:40:40 +0530

    Time: 0 ms (100%), Space: 41.7 MB (98.25%) - LeetHub

--- 3321-type-of-triangle/3321-type-of-triangle.java
+++ 3321-type-of-triangle/3321-type-of-triangle.java
@@ -1,30 +1,9 @@
 class Solution {
     public String triangleType(int[] nums) {
-        int n=nums.length;
-    
-        int pos=0;
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            if(max<nums[i]){
-                max=nums[i];
-                pos=i;
-            }
-        }
-        int sum=0;
-        for(int i=0;i<n;i++){
-            if(i!=pos){
-                sum+=nums[i];
-            }
-        }
-        if(sum<=max){
-            return "none";
-        }
-        else if(nums[0]==nums[1] && nums[1]==nums[2]){
-            return "equilateral";
-        }else if((nums[0]==nums[2] && nums[1]!=nums[2])||(nums[0]==nums[1] && nums[1]!=nums[2]) || (nums[0]!=nums[1] && nums[1]==nums[2]) ){
-            return "isosceles";
-        }
+        int a = nums[0], b = nums[1], c = nums[2];
+        if (a + b <= c || a + c <= b || b + c <= a) return "none";
+        if (a == b && b == c) return "equilateral";
+        if (a == b || b == c || a == c) return "isosceles";
         return "scalene";
-        
     }
 }
